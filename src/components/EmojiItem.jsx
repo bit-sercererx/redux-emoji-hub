79d64757cc1a3b06b47b743a75fa952e53c9ba01
ZehraaMA <zahraa@thecodepeople.io>
2024-04-07 23:17:43 +0300

    refactor the code

--- src/components/EmojiItem.jsx
+++ src/components/EmojiItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 export const EmojiItem = ({ emoji }) => {
-  console.log("🚀 ~ EmojiItem ~ emoji:", emoji);
   return (
     <div>
       <p>{emoji.name}</p>
       <p>{emoji.category}</p>
+      <p>{emoji.htmlCode}</p>
       <p dangerouslySetInnerHTML={{ __html: emoji.htmlCode[0] }}></p>
     </div>
   );
--- src/components/EmojiList.jsx
+++ src/components/EmojiList.jsx
@@ -1,8 +1,7 @@
 import React from "react";
+import { EmojiItem } from "./EmojiItem";
 
-export const EmojieList = () => {
-  // TODO: use useSelector to get the list of emojis
-
+export const EmojiList = () => {
   return (
     <div className="flex w-1/2 flex-wrap m-10 gap-12">
       {/* {use the map method to display the list of emojis and pass each emoji to the EmojiItem component} */}
