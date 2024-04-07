e455403d11bc9bed5d22adf0e21f983c3994ea54
ZehraaMA <zahraa@thecodepeople.io>
2024-04-07 22:16:54 +0300

    create emoji list component

---
+++ src/components/EmojiList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const EmojieList = () => {
+  // TODO: use useSelector to get the list of emojis
+
+  return (
+    <div className="flex w-1/2 flex-wrap m-10 gap-12">
+      {/* {use the map method to display the list of emojis and pass each emoji to the EmojiItem component} */}
+    </div>
+  );
+};
